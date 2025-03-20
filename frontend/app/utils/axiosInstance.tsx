1e5521f0de6f42f3d4382c321cce82f573487d27
ANIKET PANWAR <panwaraniket007@gmail.com>
2025-03-20 21:30:53 +0530

    update backend port

--- frontend/app/utils/axiosInstance.tsx
+++ frontend/app/utils/axiosInstance.tsx
@@ -5,7 +5,9 @@ import axios, {
 } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:10000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "https://houseofmarktech-assessment.onrender.com/",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
